680db620d0e53c070c15213407b63c1dec17ed11
Nikita <98713485+n1kitka@users.noreply.github.com>
2025-01-29 11:24:55 +0200

    Initial Commit

---
+++ ExchangeCurrencyApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExchangeCurrencyApp
+//
+//  Created by Nikita on 29.01.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
